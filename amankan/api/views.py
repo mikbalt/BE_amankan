56cf6f90bea745357eb4ca1220a6f9a5a7031888
Ikbal Taqyudin <39777937+mikbalt@users.noreply.github.com>
2025-02-23 01:38:13 +0700

    Authentication and Postgre connection already established

---
+++ amankan/api/views.py
@@ -0,0 +1,23 @@
+# amakan/api/views.py
+from rest_framework import generics
+from rest_framework.permissions import AllowAny, IsAuthenticated
+from rest_framework_simplejwt.views import TokenObtainPairView
+from django.contrib.auth import get_user_model
+from .serializers import UserSerializer, CustomTokenObtainPairSerializer
+
+User = get_user_model()
+
+class RegisterView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    permission_classes = (AllowAny,)
+    serializer_class = UserSerializer
+
+class CustomTokenObtainPairView(TokenObtainPairView):
+    serializer_class = CustomTokenObtainPairSerializer
+
+class UserProfileView(generics.RetrieveUpdateAPIView):
+    permission_classes = (IsAuthenticated,)
+    serializer_class = UserSerializer
+
+    def get_object(self):
+        return self.request.user
